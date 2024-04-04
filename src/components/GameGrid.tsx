00876d78fd7200a31583ff7ff5e044783ab4cc0d
shidhu84 <shidyenure@yahoo.com>
2024-04-05 05:17:27 +0530

    Added charka skeleton to show loading

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,11 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
-import GameCard from "./GameCard";
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import useGames from '../hooks/useGames';
+import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
+const gameCardSkeletonList = [1, 2, 3, 4, 5, 6];
 
 function GameGrid() {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
 
   return (
     <div>
@@ -11,8 +13,12 @@ function GameGrid() {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         spacing={10}
-        padding="10px"
+        padding='10px'
       >
+        {isLoading &&
+          gameCardSkeletonList.map((skeleton) => (
+            <GameCardSkeleton key={skeleton} />
+          ))}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
