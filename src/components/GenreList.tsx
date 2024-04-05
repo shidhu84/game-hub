fd3eeb588c97ac7bc207c5360b21e8c01a6f4a84
shidhu84 <shidyenure@yahoo.com>
2024-04-05 06:56:06 +0530

    Added skeleton loading for sidebar

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import useGenres from '../hooks/useGenres';
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react';
 import getCroppedImageUrl from '../services/image-url';
+const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function GenreList() {
   const { data: genres, error, isLoading } = useGenres();
+  if (error) return null;
   return (
     <List>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ListItem>
+            <SkeletonText />
+          </ListItem>
+        ))}
       {genres.map((genre) => (
         <ListItem key={genre.id} paddingY='5px'>
           <HStack>
